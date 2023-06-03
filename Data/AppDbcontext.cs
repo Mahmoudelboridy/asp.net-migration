d7ce75953677aa1d5b7bae24133015b4765caa42
mahmoud khaled <111857983+Mahmoudelboridy@users.noreply.github.com>
2023-06-03 22:48:33 +0300

    Add files via upload

---
+++ Data/AppDbcontext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using test.Models;
+
+namespace test.Data
+{
+    public class AppDbcontext : DbContext
+    {
+        public AppDbcontext(DbContextOptions<AppDbcontext> options) : base(options)
+        {
+            
+        }
+        public DbSet<item> Items { get; set; }
+    }
+}
---
+++ Models/item.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace test.Models
+{
+    public class item
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public decimal price { get; set; }
+        
+        public DateTime DateTime { get; set; } = DateTime.Now;
+    }
+}
